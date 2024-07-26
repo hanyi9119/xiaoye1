d9514f04183afb9419b4962808ae8a384a80a75d
hanyi9119 <38556778+hanyi9119@users.noreply.github.com>
2024-07-26 18:34:17 +0800

    Create memoryCheck.sh

---
+++ memoryCheck.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+echo -e "\033[33m内存超售检测开始\033[0m"
+echo -e "\033[0m====================\033[0m"
+
+# 检查是否使用了 SWAP 超售内存
+echo -e "\033[36m检查是否使用了 SWAP 超售内存\033[0m"
+memSize=$(free -m | awk '/Mem/ {print $2}')
+speed=$(dd if=/dev/zero of=/dev/null bs=1M count=$memSize 2>&1 | awk 'END {print $(NF-1)}')
+speed=$(echo $speed | awk '{printf("%.0f\n",$1)}')
+
+echo -e "\033[34m内存 IO 速度: $speed MB/s\033[0m"
+if [ $speed -lt 10000 ]; then
+    echo -e "\033[31m内存 IO 速度低于 10 GB/s\033[0m"
+    echo -e "\033[31m可能存在 SWAP 超售内存\033[0m"
+else
+    echo -e "\033[32m内存 IO 速度正常\033[0m"
+    echo -e "\033[32m未使用 SWAP 超售内存\033[0m"
+fi
+echo -e "\033[0m====================\033[0m"
+
+# 检查是否使用了气球驱动 Balloon 超售内存
+echo -e "\033[36m检查是否使用了 气球驱动 Balloon 超售内存\033[0m"
+if lsmod | grep -q virtio_balloon; then
+    echo -e "\033[31m存在 virtio_balloon 模块\033[0m"
+    echo -e "\033[31m可能使用了 气球驱动 Balloon 超售内存\033[0m"
+else
+    echo -e "\033[32m不存在 virtio_balloon 模块\033[0m"
+    echo -e "\033[32m未使用 气球驱动 Balloon 超售内存\033[0m"
+fi
+echo -e "\033[0m====================\033[0m"
+
+# 检查是否使用了 Kernel Samepage Merging (KSM) 超售内存
+echo -e "\033[36m检查是否使用了 Kernel Samepage Merging (KSM) 超售内存\033[0m"
+if [ $(cat /sys/kernel/mm/ksm/run) -eq 1 ]; then
+    echo -e "\033[31mKernel Samepage Merging 状态为 1\033[0m"
+    echo -e "\033[31m可能使用了 Kernel Samepage Merging (KSM) 超售内存\033{0m"
+else
+    echo -e "\033[32mKernel Samepage Merging 状态正常\033[0m"
+    echo -e "\033[32m未使用 Kernel Samepage Merging (KSM) 超售内存\033[0m"
+fi
+echo -e "\033[0m====================\033{0m"
