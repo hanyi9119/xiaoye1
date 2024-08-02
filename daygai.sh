cd35ccfbd9eeab060c4141735e5f61df04eff8c9
hanyi9119 <38556778+hanyi9119@users.noreply.github.com>
2024-08-02 22:08:51 +0800

    Update daygai.sh

--- daygai.sh
+++ daygai.sh
@@ -8,24 +8,24 @@ if [[ "$day" =~ ^[0-9]+$ ]] && [ "$day" -ge 1 ] && [ "$day" -le 31 ]; then
     # 确认用户输入
     echo "您输入的结算日为: $day"
 
-    # 检查vnstat.conf是否存在
+    # 配置文件路径
     VNSTAT_CONF="/etc/vnstat.conf"
+    
+    # 检查配置文件是否存在
     if [ -f "$VNSTAT_CONF" ]; then
         # 备份原配置文件
         cp "$VNSTAT_CONF" "$VNSTAT_CONF.bak"
 
-        # 更新配置文件中的MonthRotate设置
-        # 去除行首的注释符号并设置新的值
-        sed -i "s/^;\?MonthRotate[[:space:]]*[0-9]*/MonthRotate $day/" "$VNSTAT_CONF"
+        # 修改配置文件中的MonthRotate设置
+        # 保留行首的分号并设置新的值
+        sed -i "s/^;\?MonthRotate[[:space:]]*[0-9]*$/;MonthRotate $day/" "$VNSTAT_CONF"
 
         echo "vnStat配置已更新，MonthRotate已设置为 $day。"
 
-        # 输出 MonthRotate 参数前后共三行的内容
-        grep -n "^MonthRotate" "$VNSTAT_CONF" | cut -d: -f1 | while read -r line; do
-            start=$((line-1))
-            end=$((line+1))
-            awk "NR>=$start && NR<=$end" "$VNSTAT_CONF"
-        done
+        # 输出相关的三行内容
+        awk '/;MonthRotate/ {print NR-1, NR, NR+1}' "$VNSTAT_CONF" | \
+        xargs -n1 | \
+        while read -r line; do sed -n "${line}p" "$VNSTAT_CONF"; done
     else
         echo "错误：无法找到 $VNSTAT_CONF 文件。"
     fi
