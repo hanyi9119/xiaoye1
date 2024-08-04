b0d6a85038050aab92eb9257bce8ca8107eb4114
hanyi9119 <38556778+hanyi9119@users.noreply.github.com>
2024-08-04 23:37:06 +0800

    Update awsconfig.sh

--- awsconfig.sh
+++ awsconfig.sh
@@ -239,25 +239,26 @@ fi
 # 比较流量是否超过阈值
 if (( \$(echo "\$CHANGE_TO_GB > \$traffic_limit" | bc -l) )); then
     # 检查是否已有特定的iptables规则
-    if sudo iptables -L INPUT | grep -q "tcp dpt:\$ssh_port"; then
-        echo "已检测到规则，不需要再次添加。"
+    if sudo iptables -C INPUT -p tcp --dport $ssh_port -j ACCEPT 2>/dev/null; then
+       echo "已检测到规则，不需要再次添加。"
     else
-        # 备份现有iptables规则
-        sudo iptables-save > /root/awsconfig/iptables_backup.rules
+       # 备份现有iptables规则
+       sudo iptables-save > /root/awsconfig/iptables_backup.rules
 
-        # 清除所有规则
-        sudo iptables -F
+       # 清除所有规则
+       sudo iptables -F
 
-        # 允许SSH连接
-        sudo iptables -A INPUT -p tcp --dport \$ssh_port -j ACCEPT
-        sudo iptables -A OUTPUT -p tcp --sport \$ssh_port -j ACCEPT
+       # 允许SSH连接
+       sudo iptables -A INPUT -p tcp --dport $ssh_port -j ACCEPT
+       sudo iptables -A OUTPUT -p tcp --sport $ssh_port -j ACCEPT
 
-        # 拒绝其他所有流量
-        sudo iptables -A INPUT -j DROP
-        sudo iptables -A OUTPUT -j DROP
+       # 拒绝其他所有流量
+       sudo iptables -A INPUT -j DROP
+       sudo iptables -A OUTPUT -j DROP
 
-        echo "流量超限，已屏蔽所有连接，仅允许SSH连接。"
-    fi
+       echo "流量超限，已屏蔽所有连接，仅允许SSH连接。"
+fi
+
 fi
 EOF
 
