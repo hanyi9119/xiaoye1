a1dc34ea99a04e9871004a2614d2de96858bb439
hanyi9119 <38556778+hanyi9119@users.noreply.github.com>
2024-08-04 22:44:42 +0800

    Update awsconfig.sh

--- awsconfig.sh
+++ awsconfig.sh
@@ -273,7 +273,7 @@ EOF
     sudo iptables -L -n -v
     echo "流量超限时将屏蔽所有连接，仅保留SSH连接。"
     echo "查看定时任务，输入：crontab -l"
-    echo "超额流量数值保存文件 /root/awsconfig/traffic_limit_block.txt"
+    echo "超额流量数值保存文件 /root/awsconfig/traffic_limit.txt"
     echo "实时检测脚本文件 /root/awsconfig/block_traffic.sh"
     echo "实时流量数据储存文件 /root/awsconfig/block_traffic_debug.log"
     echo "大功告成！断网脚本已安装并配置完成。"
