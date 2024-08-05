c438b86d539f1a640ae6342dbd9297367a754f53
hanyi9119 <38556778+hanyi9119@users.noreply.github.com>
2024-08-05 13:37:46 +0800

    Update awsconfig.sh

--- awsconfig.sh
+++ awsconfig.sh
@@ -301,7 +301,7 @@ restore_network() {
     if [ -f /root/awsconfig/iptables_backup.rules ]; then
         # 恢复备份的iptables规则
         sudo iptables-restore < /root/awsconfig/iptables_backup.rules
-        echo "网络连接已恢复。"
+        echo "iptables规则已恢复,建议重启一下：reboot"
     else
         echo "找不到iptables规则的备份文件。无法恢复网络连接。"
     fi
